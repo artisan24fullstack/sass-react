96d1611eff1bc4a8e9ea03aa30a081f5b831358f
artisan24fullstack <denisartisanfullstack@gmail.com>
2024-06-09 09:49:35 +0200

    bug btn toggle follow

---
+++ src/components/users/following-user.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { FollowimgData } from '../../data/FollowingData'
+import FollowingList from './following-list'
+
+const FollowingUser = ({ following, setFollowing }) => {
+
+  const [showMore, setShowMore] = useState(false)
+
+
+  return (
+    <div className="following-comp">
+      <h2>Who is following you</h2>
+      {FollowimgData.map((data, id) => (
+
+        <FollowingList
+          following={following}
+          setFollowing={setFollowing}
+          data={data}
+          key={id}
+        />
+      ))}
+
+
+
+      <button className='SM-btn' onClick={() => setShowMore(true)}>Show more</button>
+    </div>
+
+  )
+}
+
+export default FollowingUser
--- src/layout/aside.jsx
+++ src/layout/aside.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { GrFormClose } from "react-icons/gr"
 import SuggestionUser from '../components/users/suggestion-user'
 import StatutUser from '../components/users/statut-user'
-
+import FollowingUser from '../components/users/following-user'
 const Aside = ({ following, setFollowing, showMenu, setShowMenu }) => {
 
   return (
@@ -17,13 +17,10 @@ const Aside = ({ following, setFollowing, showMenu, setShowMenu }) => {
       />
 
       <StatutUser />
-      {/*
-
       <FollowingUser
         following={following}
         setFollowing={setFollowing}
       />
-*/}
     </div>
   )
 }
